f5ef6b071e3a361efd799b7a723605b01aceb726
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-05-14 16:12:05 +0300

    Implemented Manufacturer methods

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,10 +4,15 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarCreateView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -16,8 +21,24 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path(
+        "manufacturer/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create"
+    ),
+    path(
+        "manufacturer/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update"
+    ),
+    path(
+        "manufacturer/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("car/create/", CarCreateView.as_view(), name="car-create"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
